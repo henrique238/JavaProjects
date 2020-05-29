78625bed18fa147988db1683a2fd9f20c8cdb855
Henrique Barreto <henriique_bm@hotmail.com>
2020-05-29 15:57:33 -0300

    first commit

---
+++ FirstSpringRestApi-2/src/test/java/com/example/FirstSpringRestApi2/FirstSpringRestApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstSpringRestApi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringRestApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
